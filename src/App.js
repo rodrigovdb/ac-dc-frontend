485da20e68d1527a5ddc9d7b0701a61bd53df0cf
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2022-07-04 14:06:58 -0400

    Initial project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from '@apollo/client';
+
+import GetAlbums from './Components/GetAlbums';
+
+import { ErrorLink, onError } from '@apollo/client/link/error'
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if(graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`)
+    })
+  }
+})
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: 'http://ac-dc.herokuapp.com/graphql' })
+])
+
+/*
+const link = from([
+  errorLink,
+  new HttpLink({ uri: 'http://localhost:3000/graphql' })
+])
+*/
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <GetAlbums />
+    </ApolloProvider>
   );
 }
 
---
+++ src/Components/GetAlbums.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+import { useQuery, gql } from '@apollo/client'
+import { LOAD_ALBUMS } from '../GraphQL/Resolvers'
+
+function GetAlbums() {
+    const { error, loading, data } = useQuery(LOAD_ALBUMS)
+    const [albums, setAlbums] = useState([])
+
+    useEffect(() => {
+        if(data) {
+            console.log(data);
+            setAlbums(data.albums)
+        }
+    }, [data])
+
+
+    return <ul>
+        {albums.map((album) => {
+            return <li>{album.name}</li>;
+        })}
+    </ul>;
+
+}
+
+export default GetAlbums;
