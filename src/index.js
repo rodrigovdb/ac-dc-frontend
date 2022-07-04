c3ad0fe579a7bc1e91483349734e06c11907adee
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2022-07-04 18:31:59 -0400

    css

--- src/index.js
+++ src/index.js
@@ -9,9 +9,15 @@ import client from './client'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1 id="pageTitle">AC/DC Studio Albums</h1>
+
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
+
+    <footer id="pageFooter">
+      by <a href="https://github.com/rodrigovdb">@rodrigovdb</a>
+    </footer>
   </React.StrictMode>
 );
 
