7fee6edc244c30f492331bc724a734688423d064
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2022-07-15 10:40:01 -0400

    Config tailwind

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './components/app/app.component'
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
